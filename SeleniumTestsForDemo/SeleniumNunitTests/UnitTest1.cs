11b8fb46c50c660046de75eeb9d5bf46b642a81b
Soumya Pillai <soumya.pillai@odido.nl>
2025-08-28 21:26:27 +0200

    first commit

---
+++ SeleniumTestsForDemo/SeleniumNunitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace SeleniumNunitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
